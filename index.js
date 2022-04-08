8f612107a749108dcd4547c1ca8d6a90972dad66
diesel <jpedro.diesel@gmail.com>
2022-04-08 09:27:47 -0300

    fix atempt 01

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
