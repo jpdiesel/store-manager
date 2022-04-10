23c86b16cbbda214a3d32b7cf1c0056817a604a4
diesel <jpedro.diesel@gmail.com>
2022-04-09 22:20:33 -0300

    fixing issues 03

--- models/connection.js
+++ models/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  host: process.env.MYSQL_HOST,
   database: 'StoreManager',
 });
 
